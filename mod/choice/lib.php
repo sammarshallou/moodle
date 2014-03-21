567ab4701f1395d22cd5cd0c73b7df17e8150fcc
sam marshall <s.marshall@open.ac.uk>
2014-03-21 12:12:40 +0000

    Patch 2 (modules)

--- mod/choice/lib.php
+++ mod/choice/lib.php
@@ -782,7 +782,6 @@ function choice_get_extra_capabilities() {
 /**
  * @uses FEATURE_GROUPS
  * @uses FEATURE_GROUPINGS
- * @uses FEATURE_GROUPMEMBERSONLY
  * @uses FEATURE_MOD_INTRO
  * @uses FEATURE_COMPLETION_TRACKS_VIEWS
  * @uses FEATURE_GRADE_HAS_GRADE
@@ -794,7 +793,6 @@ function choice_supports($feature) {
     switch($feature) {
         case FEATURE_GROUPS:                  return true;
         case FEATURE_GROUPINGS:               return true;
-        case FEATURE_GROUPMEMBERSONLY:        return true;
         case FEATURE_MOD_INTRO:               return true;
         case FEATURE_COMPLETION_TRACKS_VIEWS: return true;
         case FEATURE_COMPLETION_HAS_RULES:    return true;
